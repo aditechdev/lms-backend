891deb69a074f520373a91988e7f735db0283707
Aditya Anand <aditechdevofficial@gmail.com>
2023-05-24 11:07:10 +0530

    :sparkles: Created curd API for bootcamp

--- server.js
+++ server.js
@@ -8,6 +8,42 @@ dotenv.config({
 
 const app = express();
 
+app.get('/api/v1/bootcamp', (req, res) => { 
+    // res.send("<h1>HELLO form Express</h1>");
+    res.status(200).json({
+        success: true,
+        msg: "Show all bootcamp"
+    });
+});
+app.post('/api/v1/bootcamp', (req, res) => { 
+    // res.send("<h1>HELLO form Express</h1>");
+    res.status(200).json({
+        success: true,
+        msg: "Create new bootcamp"
+    });
+});
+app.put('/api/v1/bootcamp/:id', (req, res) => { 
+    // res.send("<h1>HELLO form Express</h1>");
+    res.status(200).json({
+        success: true,
+        msg: `Update bootcamp ${req.params.id}`
+    });
+});
+app.get('/api/v1/bootcamp/:id', (req, res) => { 
+    // res.send("<h1>HELLO form Express</h1>");
+    res.status(200).json({
+        success: true,
+        msg: `Get bootcamp ${req.params.id}`
+    });
+});
+app.delete('/api/v1/bootcamp/:id', (req, res) => { 
+    // res.send("<h1>HELLO form Express</h1>");
+    res.status(200).json({
+        success: true,
+        msg: `Delete bootcamp ${req.params.id}`
+    });
+});
+
 const PORT = process.env.PORT || 5000;
 
 
