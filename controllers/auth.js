a2c7f5c9b4d08472611496a0a4b884def88c9246
Aditya Anand <aditechdevofficial@gmail.com>
2023-05-30 23:08:38 +0530

    :bug: sign in and register
    Becrypt, JWT

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,7 +3,7 @@ const User = require('../models/User');
 const asyncHandler = require('../middleware/async');
 
 //@desc     Register User
-//@route    Get Api '/api/v1/auth/register'
+//@route    Post Api '/api/v1/auth/register'
 //@acess    public
 exports.register = asyncHandler(async (req, res, next) => {
 
@@ -15,6 +15,41 @@ exports.register = asyncHandler(async (req, res, next) => {
         password,
         role
     })
+
+    const token = user.getSignedJwtToken();
+
+    res.status(200).json({
+        success: true, token
+    });
+
+});
+//@desc     Login User
+//@route    Post Api '/api/v1/auth/register'
+//@acess    public
+exports.signIn = asyncHandler(async (req, res, next) => {
+
+    const { email, password } = req.body;
+    
+    //Validate email and password
+    if (!email || !password) {
+
+        return next(new ErrorResponse("Please provide an email and password", 400));
+        
+    }
+
+    // Check for user
+    const user = await User.findOne({ email }).select('+password');
+    
+    if (!user) {
+        return next(new ErrorResponse("Invalid credentials", 401));
+        
+    }
+
+    // Check if password matches
+    const isMatch = await user.matchPassword(password);
+    if (!isMatch) {
+        return next(new ErrorResponse("Invalid credentials", 401));
+    }
 
     const token = user.getSignedJwtToken();
 
--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,10 @@
 const express = require('express');
 
-const { register } = require('../controllers/auth');
+const { register, signIn } = require('../controllers/auth');
 
 const router = express.Router();
 
 router.post('/register', register);
+router.post('/signin', signIn);
 
 module.exports = router;
