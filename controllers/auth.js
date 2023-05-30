7f0c0629784cde03a620125cd973e9861b56a4e3
Aditya Anand <aditechdevofficial@gmail.com>
2023-05-31 00:17:34 +0530

    :rocket: Get User Detail API done

--- controllers/auth.js
+++ controllers/auth.js
@@ -89,3 +89,47 @@ const sendTokenResponse = (user, statusCode, res) => {
         });
 
 }
+
+//@desc     Get Current login User
+//@route    Post Api '/api/v1/auth/me'
+//@acess    public
+exports.getMe = asyncHandler(async (req, res, next) => {
+
+    const user = await User.findById(req.user.id);
+
+    res.status(200).json({
+        status: 200,
+        data: user
+    });
+
+    // const { email, password } = req.body;
+
+    // //Validate email and password
+    // if (!email || !password) {
+
+    //     return next(new ErrorResponse("Please provide an email and password", 400));
+
+    // }
+
+    // // Check for user
+    // const user = await User.findOne({ email }).select('+password');
+
+    // if (!user) {
+    //     return next(new ErrorResponse("Invalid credentials", 401));
+
+    // }
+
+    // // Check if password matches
+    // const isMatch = await user.matchPassword(password);
+    // if (!isMatch) {
+    //     return next(new ErrorResponse("Invalid credentials", 401));
+    // }
+
+    // // const token = user.getSignedJwtToken();
+
+    // // res.status(200).json({
+    // //     success: true, token
+    // // });
+    // sendTokenResponse(user, 200, res)
+
+});
--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const { protect } = require('../middleware/auth');
 
-const { register, signIn } = require('../controllers/auth');
+const { register, signIn, getMe } = require('../controllers/auth');
 
 const router = express.Router();
 
 router.post('/register', register);
 router.post('/signin', signIn);
+router.get('/me',protect, getMe);
 
 module.exports = router;
